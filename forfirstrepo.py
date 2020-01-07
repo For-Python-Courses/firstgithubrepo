2ec2ad9f5211cd2393bc1f02ec5aaeb1b3e87eba
Lou Marco <loumarco@hotmail.com>
2020-01-07 11:07:55 -0600

    Adding .gitignore and saymyname.py

--- forfirstrepo.py
+++ forfirstrepo.py
@@ -1,5 +1,5 @@
 ''' Just a small program to add to the repo '''
 from saymyname import say_name
-
+       
 say_name( )
 print("My first git repo!!!")
---
+++ saymyname.py
@@ -0,0 +1,12 @@
+''' ANother file for the git repo
+    Showing generation of cache files that should
+    be ignored when saving to a git repo
+'''
+def say_name( ):
+    print ("I'm Lou")
+
+def main( ):
+    say_name( )
+
+if __name__ == '__main__':
+    main()
